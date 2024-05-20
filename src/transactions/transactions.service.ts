93c7a1bea8ac296a0f0f752cc062e676bfb8e2e4
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-05-20 17:48:25 -0600

    connect user entity to transactions service

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Transaction } from './transaction.entity';
+import { User } from './user.entity';
 
 @Injectable()
 export class TransactionsService {
   constructor(
     @InjectRepository(Transaction)
     private transactionRepository: Repository<Transaction>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
   findAll(): Promise<Transaction[]> {
